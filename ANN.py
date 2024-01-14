dd0c72663b107fd24dc52e78a7920641426cd7b5
chodii <chodoraj1@gmail.com>
2024-01-14 19:33:57 +0100

    Dedictionarization of ANN
    Neurons' outputs moved from ANN into it's separate matrix, ANN contains now only weights and is a 3-dimensional array

--- ANN.py
+++ ANN.py
@@ -17,6 +17,10 @@ import statistics
 
 import io
 
+# --------
+# PLOTING
+# --------
+
 def dictionarize_X_y(X, y, y_range = range(1,6,1)):
     len_X = len(X)# since array should be fixed, O(1); just felt wrong not to save
     Xy = {yi:[[] for _ in range(len_X)] for yi in y_range}# prepare dictionary
@@ -60,8 +64,6 @@ def get_data(filepath="../data/tren_data2___08.txt", columns=3):
 
 
 def main():
-    
-    
     dim = (2,5,10, 5)#, 19
     EPOCHS = 1000
     learning_rate = 0.8
@@ -103,9 +105,8 @@ def main():
     pts = []
     for _ in range(take_cover):
         pt = [random.uniform(-2.0,2.0), random.uniform(-2, 2)] 
-        out = feed_me(ann, pt, dim)
+        vectout = feed_me(ann, pt, dim)[-1]
         
-        vectout = vectorize_neuron(out)
         pt3d = [pt[0], pt[1], max(vectout)]
         scout = scalaratize_output(vectout)
         pts.append((pt3d, scout, -2 if scout == -1 else -1))
@@ -151,9 +152,8 @@ def test(ann, dim, test_keys, input_data, output_data):
     results = []
     for k in test_keys:
         targ_out, targ_inp = get_target(dim, output_data, input_data, k)
-        out = feed_me(ann, targ_inp, dim)
+        vectout = feed_me(ann, targ_inp, dim)[-1]
         
-        vectout = vectorize_neuron(out)
         accuracy = prediction_accuracy(vectout, targ_out)
         accuracies.append(accuracy)
         
@@ -187,23 +187,23 @@ def train(ann, dim, train_keys, validation_keys, input_data, output_data, EPOCHS
     
     avg_accuracies=[]
     best_acc = None
-    b = 0
-    target_outputs = []
+    #b = 0
+    #target_outputs = []
     for i in range(EPOCHS):
         print(".", end="")
         #outpus = []
         for k in train_keys:
             targ_out, targ_inp = get_target(dim, output_data, input_data, k)
-            out = feed_me(ann, targ_inp, dim)
+            out_matrix = feed_me(ann, targ_inp, dim)
             #outpus.append(out)
-            target_outputs.append(targ_out)
             
-            b += 1
-            if b%batch_size == 0:
-                #ann = propagation(ann, target_outputs, learning_rate)
-                target_outputs = []
+            #target_outputs.append(targ_out)
+            #b += 1
+            #if b%batch_size == 0:
+            #    #ann = propagation(ann, target_outputs, learning_rate)
+            #    target_outputs = []
                 
-            ann = back_propagation(ann, targ_out, learning_rate)#,dim
+            ann = back_propagation(ann, out_matrix, targ_out, learning_rate)#,dim
         avg_acc = validation(ann, dim, validation_keys, input_data, output_data)
         avg_accuracies.append(avg_acc)
         #    return best_acc, accuracy_avg, i, avg_accuracies
@@ -215,9 +215,8 @@ def validation(ann, dim, validation_keys, input_data, output_data):
     accuracies = []
     for k in validation_keys:
         targ_out, targ_inp = get_target(dim, output_data, input_data, k)
-        out = feed_me(ann, targ_inp, dim)
+        vectout = feed_me(ann, targ_inp, dim)[-1]
         
-        vectout = vectorize_neuron(out)
         accuracy = prediction_accuracy(vectout, targ_out)
         accuracies.append(accuracy)
     avg_acc = float(sum(accuracies))/len(accuracies)
@@ -240,37 +239,42 @@ def prediction_accuracy(outvect, targ_out):
     accuracy = (2 - sum(err_vect))/2
     return accuracy
 
-def vectorize_neuron(out):
-    outvect = [0] * len(out)
-    for i in range(len(out)):
-        outvect[i] = out[i]["output"]
-    return outvect
 
 # ----------------------------------------------------------------
 #   Functionality:
 # ----------------------------------------------------------------
 
+def create_output_matrix(dim):
+    output_network = []
+    for d in dim:
+        layer = [0 for _ in range(d)]
+        output_network.append(layer)
+    return output_network
+
+
 def feed_me(ann, targ_inp, dim):
+    output_matrix = create_output_matrix(dim)
     for i in range(len(targ_inp)):
-        ann[0][i]["output"] = targ_inp[i]
+        output_matrix[0][i] = targ_inp[i]
     
     for l in range(1, len(ann), 1):
-        for neuron in ann[l]:
-            net = sum_inputs(neuron["weight"], ann[l-1])
-            neuron["output"] = sigma(net)
-    return ann[len(ann)-1]
+        for i in range(len(ann[l])):
+            neuron = ann[l][i]
+            net = sum_inputs(neuron, output_matrix[l-1])
+            output_matrix[l][i] = sigma(net)
+    return output_matrix
     
         
 def print_ann(ann):
     for layer in ann:
         for n in layer:
-            print(n["weight"])
+            print(n)
             
 
 def sum_inputs(weights, inputs):
     x = 0
     for i in range(len(inputs)):
-        x += weights[i] * inputs[i]["output"]
+        x += weights[i] * inputs[i]
     x += weights[-1]# +bias
     return x
 
@@ -326,8 +330,8 @@ def construct_neuron(prev_dim):
     neuron_weights = [0.0]* (prev_dim +1)# +1 for bias
     for i in range(prev_dim):
         neuron_weights[i] = random.uniform(0, 1)
-    neuron_output = 0
-    return {"weight":neuron_weights,"output":neuron_output}
+    #neuron_output = 0
+    return neuron_weights
 
 # NN[
 # L  [
@@ -344,13 +348,14 @@ def construct_neuron(prev_dim):
 # ----------------------------------------------------------------
 
 #back_prop_2
-def back_propagation(ann, target_output, learning_rate):#, dim len(dim) == len(ann)+1
+def back_propagation(ann, out_matrix, target_output, learning_rate):#, dim len(dim) == len(ann)+1
     """
         network propagation
     """
     deltas_cpy = []
     for lay_ix in range(len(ann)-1, 1, -1):# start, stop, step
         layer = ann[lay_ix]
+        
         prev_layer = ann[lay_ix-1]
         # for each layer and layer before it
         err_term = 0
@@ -358,32 +363,32 @@ def back_propagation(ann, target_output, learning_rate):#, dim len(dim) == len(a
         # for each neuron in the layer
         for n_ix in range(len(layer)):
             if lay_ix == len(ann)-1:
-                err_term = error_term_output(t = target_output[n_ix], o = layer[n_ix]["output"])
+                err_term = error_term_output(t = target_output[n_ix], o = out_matrix[lay_ix][n_ix])
             else:
-                err_term = error_term_hidden(o_j = layer[n_ix]["output"], deltas_next=deltas_cpy, layer_next=ann[lay_ix+1], neuron_ix=n_ix)
+                err_term = error_term_hidden(o_j = out_matrix[lay_ix][n_ix], deltas_next=deltas_cpy, layer_next=ann[lay_ix+1], neuron_ix=n_ix)
             deltas.append(err_term)
-            propagate_weights(ann, layer, prev_layer, err_term, n_ix, lay_ix, learning_rate)
-        #delta = delta_w(o_j = layer[n_ix]["output"], delta_next = err_term)
+            propagate_weights(ann, out_matrix, lay_ix, lay_ix-1, err_term, n_ix, lay_ix, learning_rate)
         deltas_cpy = deltas
     return ann
 
-def propagate_weights(ann, source_layer, destination_layer, err_term, n_ix, lay_ix, learning_rate):
+def propagate_weights(ann, output_matrix, source_ix, destination_ix, err_term, n_ix, lay_ix, learning_rate):
     """
         neuron's weights propagation
     """
-    for prev_n_ix in range(len(destination_layer)):# previous layer <-|
+    destination_layer = ann[destination_ix]
+    for prev_n_ix in range(len(ann[destination_ix])):# previous layer <-|
         if lay_ix >= 1:
-            delt_w = delta_w(destination_layer[prev_n_ix]["output"], err_term, learning_rate)
+            delt_w = delta_w(output_matrix[destination_ix][prev_n_ix], err_term, learning_rate)
         else:
-            delt_w = delta_w(destination_layer[prev_n_ix]["output"], err_term, learning_rate)
-        old_w = source_layer[n_ix]["weight"][prev_n_ix]
-        ann[lay_ix][n_ix]["weight"][prev_n_ix] = old_w + delt_w
+            delt_w = delta_w(output_matrix[destination_ix][prev_n_ix], err_term, learning_rate)
+        old_w = ann[source_ix][n_ix][prev_n_ix]
+        ann[lay_ix][n_ix][prev_n_ix] = old_w + delt_w
     
     bias = 1
-    #bias = source_layer[n_ix]["weight"][-1]# slower learning
+    #bias = source_layer[n_ix][-1]# slower learning
     delt_wb = delta_w(bias, err_term, learning_rate)
-    old_wb = source_layer[n_ix]["weight"][-1]
-    ann[lay_ix][n_ix]["weight"][-1] = old_wb + delt_wb
+    old_wb = ann[source_ix][n_ix][-1]
+    ann[lay_ix][n_ix][-1] = old_wb + delt_wb
     
 
 def delta_w(o_j, delta_next, learning_rate = 0.5):
@@ -410,7 +415,7 @@ def error_term_hidden(o_j, deltas_next, layer_next, neuron_ix):
     dot_product=0
     for i in range(len(deltas_next)):
         d = deltas_next[i]
-        w = layer_next[i]["weight"][neuron_ix]
+        w = layer_next[i][neuron_ix]
         dot_product += d * w
     delta = (1 - o_j) * o_j * dot_product
     return delta
